cd63d2538b95c30b05b95ca43c682d36297c25ed
jyotipixolo <jyoti.pixolo@gmail.com>
2018-02-05 02:27:55 +0530

    Creation of gulpfile and watch function modification

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence = require('run-sequence');
+
+// ... variables
+// var autoprefixerOptions = {
+//   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+// };
+var paths = {
+  sass: ['./www/scss/**/*.scss']
+};
+
+gulp.task("serve:before", ['watch']);
+
+gulp.task('default', ['sass', 'autoprefix','processes','watch'], function () { 
+  console.log('I am gulp'); 
+  gulp.watch(paths.sass, ['processes']);
+});
+
+gulp.task('sass', function (done) {
+  console.log('I am called secondly');
+  gulp.src('./www/scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(cleanCss({
+      keepSpecialComments: 0
+    }))
+
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('autoprefix', function (){
+  console.log('I am called firstly');
+  gulp.src('./www/css/ionic.app.css')
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('./www/css/'))
+});
+
+gulp.task('processes', function () {
+  console.log('processes');
+  runSequence('sass', 'autoprefix');
+});
+
+
+  gulp.watch(paths.sass, ['processes']);
+
